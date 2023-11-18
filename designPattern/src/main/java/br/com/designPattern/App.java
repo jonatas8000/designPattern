2160dfe2ed449238e457c2b88f0806d0cae0b76d
jonatas8000 <mjonatas@ymail.com>
2023-11-17 23:19:02 -0300

    configura inicial

---
+++ designPattern/src/main/java/br/com/designPattern/App.java
@@ -0,0 +1,13 @@
+package br.com.designPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
